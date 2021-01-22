2989797a9e01d7d4bb4f9b8b6c366a0008d29edb
cdanielss <cdanielss1973@outlook.com>
2021-01-22 20:49:51 -0300

    Apagando arquivos

--- TAD1.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-typedef struct pessoa Pessoa;
-typedef struct docente Docente;
-typedef struct discente Discente;
-
--- main.c
+++
@@ -1,7 +0,0 @@
-#include<stidio.h>
-
-int main()
-{
-
-    return 0;
-}
